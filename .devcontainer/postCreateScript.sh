cd61ff3c02cd319457dd0d6c94ed7207b9dff284
Malix-off <alix.brunet@efrei.net>
2024-06-06 00:25:49 +0000

    almost ready

---
+++ .devcontainer/postCreateScript.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# uv
+
+## install
+
+uv venv && source .venv/bin/activate && uv pip sync requirements.txt
+
+## forbid pip
+
+### disclaimer
+
+alias pip="echo \"pip is forbidden, use 'uv pip' instead\""
+
+### force
+
+alias force_pip="/usr/local/bin/pip"
+
+# dev server
+
+fastapi dev src/main.py

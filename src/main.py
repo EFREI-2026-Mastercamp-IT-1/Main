0a16934a585da73e145396b66a235ce6ea9e3c12
Martin Kang <88689251+kangmartin@users.noreply.github.com>
2024-06-07 20:43:12 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
 
+from functions import *
+
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/stations")
+def get_stations():
+    stations,liaisons = read_metro_file('./Version1/metro.txt')
+    read_pospoints_file('./Version1/pospoints.txt', stations)
+    return stations
+
+@app.get("/liaisons")
+def get_liaisons():
+    stations,liaisons = read_metro_file('./Version1/metro.txt')
+    return liaisons
+
+
+

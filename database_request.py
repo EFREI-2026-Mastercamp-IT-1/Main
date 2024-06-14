d4c3718e8556f333ab16a5d3d202487a7ca0f476
Maxime Boulle <maxime.boulle1@gmail.com>
2024-06-14 09:29:13 +0200

    query fix

--- database_request.py
+++ database_request.py
@@ -28,12 +28,6 @@ def get_metro_pathways():
             cursor.execute("""
             SELECT from_stop_id, to_stop_id, min_transfer_time
             FROM transfers  
-                JOIN stops ON stops.stop_id = transfers.from_stop_id
-                JOIN stop_times ON stop_times.stop_id = stops.stop_id
-                JOIN trips ON trips.trip_id = stop_times.trip_id
-                JOIN routes ON routes.route_id = trips.route_id
-            WHERE routes.route_type = 1
-            GROUP BY stops.stop_name
             """)
             result = cursor.fetchall()
             return result

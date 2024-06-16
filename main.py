2c4e371ffcb9773cee0a3b3631ec40e9621f727a
Martin Kang <88689251+kangmartin@users.noreply.github.com>
2024-06-16 17:25:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,47 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from custom_parser import *
-
-
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
+import sqlite3
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+class Stop(BaseModel):
+    stop_id: str
+    stop_sequence: int
+    lon: float
+    lat: float
+
+def get_db_connection():
+    conn = sqlite3.connect('mon_database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:8000",
+        "http://localhost:63342"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-agency_list, routes_list, trips_list, calendar_list, calendar_dates_list, stop_times_list, stops_list, transfer_list, stop_extensions_list, pathways_list = parse_all()
+@app.get("/stops", response_model=List[Stop])
+def read_stops():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ligne5 ORDER BY stop_sequence")
+    stops = cursor.fetchall()
+    return [Stop(**dict(stop)) for stop in stops]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/agency")
-async def get_agency():
-    return agency_list
-
-@app.get("/routes")
-async def get_routes():
-    return routes_list
-
-@app.get("/trips")
-async def get_trips():
-    return trips_list
-
-@app.get("/calendar")
-async def get_calendar():
-    return calendar_list
-
-@app.get("/calendar_dates")
-async def get_calendar_dates():
-    return calendar_dates_list
-
-@app.get("/stop_times")
-async def get_stop_times():
-    return stop_times_list
-
-@app.get("/stops")
-async def get_stops():
-    return stops_list
-
-@app.get("/transfer")
-async def get_transfer():
-    return transfer_list
-
-@app.get("/stop_extensions")
-async def get_stop_extensions():
-    return stop_extensions_list
-
-@app.get("/pathways")
-async def get_pathways():
-    return pathways_list
+@app.get("/stop/{stop_id}", response_model=Stop)
+def read_stop(stop_id: str):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM ligne5 WHERE stop_id = ?", (stop_id,))
+    stop = cursor.fetchone()
+    if stop is None:
+        raise HTTPException(status_code=404, detail="Stop not found")
+    return Stop(**dict(stop))

c79d209f48c308644155cc0df4dfc4de1b045349
Malix-off <alixbrunetcontact@gmail.com>
2024-06-20 19:42:23 +0000

    enhance: classes to SQLModel v0.1

--- classes.py
+++ classes.py
@@ -1,115 +1,163 @@
-class agency():
-    def __init__(self, agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_email,agency_fare_url):
-        self.agency_id = agency_id
-        self.agency_name = agency_name
-        self.agency_url = agency_url
-        self.agency_timezone = agency_timezone
-        self.agency_lang = agency_lang
-        self.agency_phone = agency_phone
-        self.agency_email = agency_email
-        self.agency_fare_url = agency_fare_url
+from sqlmodel import SQLModel, Field
 
-class route():
-    def __init__(self, route_id, agency_id, route_short_name, route_long_name, route_desc, route_type, route_url, route_color, route_text_color,route_sort_order):
-        self.route_id = route_id
-        self.agency_id = agency_id
-        self.route_short_name = route_short_name
-        self.route_long_name = route_long_name
-        self.route_desc = route_desc
-        self.route_type = route_type
-        self.route_url = route_url
-        self.route_color = route_color
-        self.route_text_color = route_text_color
-        self.route_sort_order = route_sort_order
-
-class trips():
-    def __init__(self, route_id,service_id,trip_id,trip_headsign,trip_short_name,direction_id,block_id,shape_id,wheelchair_accessible,bikes_allowed):
-        self.route_id = route_id
-        self.service_id = service_id
-        self.trip_id = trip_id
-        self.trip_headsign = trip_headsign
-        self.trip_short_name = trip_short_name
-        self.direction_id = direction_id
-        self.block_id = block_id
-        self.shape_id = shape_id
-        self.wheelchair_accessible = wheelchair_accessible
-        self.bikes_allowed = bikes_allowed
-
-class calendar():
-    def __init__(self, service_id, monday, tuesday, wednesday, thursday, friday, saturday, sunday, start_date, end_date):
-        self.service_id = service_id
-        self.monday = monday
-        self.tuesday = tuesday
-        self.wednesday = wednesday
-        self.thursday = thursday
-        self.friday = friday
-        self.saturday = saturday
-        self.sunday = sunday
-        self.start_date = start_date
-        self.end_date = end_date
-
-class calendar_dates():
-    def __init__(self, service_id, date, exception_type):
-        self.service_id = service_id
-        self.date = date
-        self.exception_type = exception_type
-
-class stop_times():
-    def __init__(self, trip_id,arrival_time,departure_time,stop_id,stop_sequence,pickup_type,drop_off_type,local_zone_id,stop_headsign,timepoint):
-        self.trip_id = trip_id
-        self.arrival_time = arrival_time
-        self.departure_time = departure_time
-        self.stop_id = stop_id
-        self.stop_sequence = stop_sequence
-        self.pickup_type = pickup_type
-        self.drop_off_type = drop_off_type
-        self.local_zone_id = local_zone_id
-        self.stop_headsign = stop_headsign
-        self.timepoint = timepoint
-
-class stops():
-    def __init__(self, stop_id,stop_code,stop_name,stop_desc,stop_lon,stop_lat,zone_id,stop_url,location_type,parent_station,stop_timezone,level_id,wheelchair_boarding,platform_code):
-        self.stop_id = stop_id
-        self.stop_code = stop_code
-        self.stop_name = stop_name
-        self.stop_desc = stop_desc
-        self.stop_lat = stop_lat
-        self.stop_lon = stop_lon
-        self.zone_id = zone_id
-        self.stop_url = stop_url
-        self.location_type = location_type
-        self.parent_station = parent_station
-        self.stop_timezone = stop_timezone
-        self.wheelchair_boarding = wheelchair_boarding
-        self.level_id = level_id
-        self.platform_code = platform_code
-
-class transfer():
-    def __init__(self, from_stop_id,to_stop_id,transfer_type,min_transfer_time):
-        self.from_stop_id = from_stop_id
-        self.to_stop_id = to_stop_id
-        self.transfer_type = transfer_type
-        self.min_transfer_time = min_transfer_time
-
-class stop_extensions():
-    def __init__(self,object_id,object_system,object_code):
-        self.object_id = object_id
-        self.object_system = object_system
-        self.object_code = object_code
-
-class pathways():
-    def __init__(self,pathway_id,from_stop_id,to_stop_id,pathway_mode,is_bidirectional,length,traversal_time,stair_count,max_slope,min_width,signposted_as,reversed_signposted_as):
-        self.pathway_id = pathway_id
-        self.from_stop_id = from_stop_id
-        self.to_stop_id = to_stop_id
-        self.pathway_mode = pathway_mode
-        self.is_bidirectional = is_bidirectional
-        self.length = length
-        self.traversal_time = traversal_time
-        self.stair_count = stair_count
-        self.max_slope = max_slope
-        self.min_width = min_width
-        self.signposted_as = signposted_as
-        self.reversed_signposted_as = reversed_signposted_as
+ID_REGEX = r'^$'
+"""
+TODO: id regex
+"""
 
 
+class Agency(SQLModel, table=True):
+    agency_id: str = Field(primary_key=True, nullable=False)
+    agency_name: str = Field(nullable=False)
+    agency_url: str = Field(nullable=False)
+    agency_timezone: str = Field(
+        nullable=False, const=True, default="Europe/Paris"
+    )
+    agency_lang: str
+    agency_phone: str
+    agency_email: str
+    agency_fare_url: str
+
+
+class Calendar_Date(SQLModel, table=True):
+
+    service_id: str = Field(nullable=False)
+    """
+    TODO: Chose where to put the primary key
+    """
+
+    date: int = Field(nullable=False)
+    """
+    TODO: Date in YYYYMMDD format
+    """
+
+    exception_type: int = Field(nullable=False)
+    """
+    TODO: 1 when POSITIVE, 2 when NEGATIVE
+    """
+
+
+class Calendar(SQLModel, table=True):
+
+    service_id = Field(nullable=False)
+    """
+    TODO: Chose where to put the primary key
+    """
+
+    monday: bool = Field(nullable=False)
+    tuesday: bool = Field(nullable=False)
+    wednesday: bool = Field(nullable=False)
+    thursday: bool = Field(nullable=False)
+    friday: bool = Field(nullable=False)
+    saturday: bool = Field(nullable=False)
+    sunday: bool = Field(nullable=False)
+
+    start_date: str = Field(nullable=False)
+    """
+    TODO: Date in YYYYMMDD format
+    """
+
+    end_date: str = Field(nullable=False)
+    """
+    TODO: Date in YYYYMMDD format
+    """
+
+
+class Pathway(SQLModel, table=True):
+    pathway_id: str = Field(primary_key=True, nullable=False)
+    from_stop_id: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    to_stop_id: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    pathway_mode: int = Field(nullable=False, ge=1, le=6)
+    is_bidirectional: bool = Field(nullable=False)
+    length: float = Field(ge=0)
+    traversal_time: int = Field(ge=0)
+    stair_count: int = Field(ge=0)
+    max_slope: float
+    min_width: float
+    signposted_as: str
+    reversed_signposted_as: str
+
+
+class Route(SQLModel, table=True):
+    HEXA_REGEX = r'^[0-9A-Fa-f]{6}$'
+
+    route_id: str = Field(primary_key=True, nullable=False)
+    agency_id: str = Field(foreign_key="Agency.id", nullable=False)
+    route_short_name: str = Field(max_length=9, nullable=False)
+    route_long_name: str = Field(nullable=False)
+    route_desc: str
+    route_type: int = Field(nullable=False, ge=0, le=7)
+    route_url: str
+    route_color: str = Field(min_length=6, max_length=6,
+                             regex=HEXA_REGEX, default="000000")
+    route_text_color: str = Field(
+        min_length=6, max_length=6, regex=HEXA_REGEX, default="FFFFFF")
+    route_sort_order: int = Field(ge=0)
+
+
+class Stop_Extension(SQLModel, table=True):
+    object_id: str = Field(nullable=False)
+    object_system: str
+    object_code: str
+
+
+class Stop_Time(SQLModel, table=True):
+    TIME_REGEX = r'^\d{2}:\d{2}:\d{2}$'
+
+    trip_id: str = Field(foreign_key="Trip.trip_id", nullable=False)
+    arrival_time: str = Field(nullable=False, regex=TIME_REGEX)
+    departure_time: str = Field(nullable=False, regex=TIME_REGEX)
+    stop_id: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    stop_sequence: int = Field(nullable=False, ge=0)
+    pickup_type: bool
+    drop_off_type: bool
+    local_zone_id: str
+    stop_headsign: str
+    timepoint: bool
+
+
+class Stop(SQLModel, table=True):
+    stop_id: str = Field(primary_key=True, nullable=False, )
+    stop_code: str
+    stop_name: str = Field(nullable=False)
+    stop_desc: str
+    stop_lon: float = Field(nullable=False)
+    stop_lat: float = Field(nullable=False)
+    zone_id: int = Field(nullable=False, ge=1, le=101)
+    stop_url: str
+    location_type: int = Field(ge=0, le=2)
+    parent_station: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    wheelchair_boarding: int = Field(ge=0, le=2)
+    stop_timezone: str
+    level_id: int
+    platform_code: str
+
+
+class Transfer(SQLModel, table=True):
+    from_stop_id: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    to_stop_id: str = Field(foreign_key="Stop.stop_id", nullable=False)
+    transfer_type: int = Field(nullable=False)
+    min_transfer_time: int = Field(ge=0)
+
+
+class Trip(SQLModel, table=True):
+    route_id: str = Field(foreign_key="Route.route_id", nullable=False)
+
+    service_id: str = Field(nullable=False)
+    """
+    TODO: Chose where to put the primary key
+    """
+
+    trip_id: str = Field(primary_key=True, nullable=False)
+    trip_headsign: str
+    trip_short_name: str
+    direction_id: int = Field(ge=0, le=1)
+
+    block_id: str
+    """
+    No idea what this refers to
+    """
+
+    shape_id: str = Field(nullable=False)
+    wheelchair_accessible: int = Field(ge=0, le=2)
+    bikes_allowed: int = Field(ge=0, le=2)

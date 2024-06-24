2053b9cca5730114f775e56a7abc3b33f4f6e7f0
Martin Kang <martin.kang@efrei.net>
2024-06-24 16:23:28 +0200

    add CORS

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://localhost:4000",
+        "http://localhost:5173"
     ],
     allow_credentials=True,
     allow_methods=["*"],
